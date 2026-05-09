4fcb814c5eefa59d67d683a3f8697c77608d83a0
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-05-10 01:25:24 +0800

    Update ipq-script.sh

--- scripts/ipq-script.sh
+++ scripts/ipq-script.sh
@@ -2,7 +2,20 @@
 sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/10.1.1.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i "s/hostname='.*'/hostname='OpenWrt'/g" package/base-files/files/bin/config_generate
-mv $GITHUB_WORKSPACE/patch/zz-$IPQ_TARGET.sh package/base-files/files/etc/uci-defaults/zz-ipq.sh
+
+
+IPQ_TARGET=$(grep -o 'CONFIG_TARGET_qualcommax_[^=]*' .config | sed -n 's/CONFIG_TARGET_qualcommax_//p' | head -n1)
+if grep -Eq "luci-app-kwrt=y" .config; then
+    mv $GITHUB_WORKSPACE/vm/libwrt-istore vermagic
+	mv $GITHUB_WORKSPACE/patch/998-istore-$IPQ_TARGET.sh package/base-files/files/etc/uci-defaults/998-ipq.sh
+else
+    mv $GITHUB_WORKSPACE/vm/libwrt-$IPQ_TARGET vermagic
+	mv $GITHUB_WORKSPACE/patch/998-$IPQ_TARGET.sh package/base-files/files/etc/uci-defaults/998-ipq.sh
+fi
+if grep -Eq "frpc=y" .config; then
+	mv $GITHUB_WORKSPACE/patch/998-z-frpc.sh package/base-files/files/etc/uci-defaults/998-z-frpc.sh
+fi
+
 # 调整NSS驱动q6_region内存区域预留大小（ipq6018.dtsi默认预留85MB，ipq6018-512m.dtsi默认预留55MB，带WiFi必须至少预留54MB，以下分别是改成预留16MB、32MB、64MB和96MB）
 # sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x01000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
 # sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x02000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
