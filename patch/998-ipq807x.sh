bcab34b3617e32231635ffa24137206603a1c9ad
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-05-19 10:55:53 +0800

    Create 998-ipq807x.sh

---
+++ patch/998-ipq807x.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#uci set luci.main.mediaurlbase='/luci-static/bootstrap'
+#uci commit luci
+
+sed -i '/passwall/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/nss/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/video/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/qualcommax/d' /etc/apk/repositories.d/distfeeds.list
+sed -i 's#downloads.immortalwrt.org#mirrors.vsean.net/openwrt#g' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://downloads.openwrt.org/snapshots/targets/qualcommax/ipq807x/kmods/6.12.87-1-467536ea7caaffdcf2daf461fb4183a9/packages.adb' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.vsean.net/openwrt/snapshots/targets/qualcommax/ipq807x/packages/packages.adb' /etc/apk/repositories.d/distfeeds.list
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+
+# wifi设置
+uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
+uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')
+if uci -q get wireless.default_radio2 >/dev/null; then
+    uci set wireless.default_radio2.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')
+fi
+#uci set wireless.radio0.txpower='20'
+#uci set wireless.default_radio0.encryption='sae-mixed'
+#uci set wireless.default_radio1.encryption='sae-mixed'
+#uci set wireless.default_radio0.key='123456qwerty'
+#uci set wireless.default_radio1.key='123456qwerty'
+uci commit wireless
+#uci set network.usbwan=interface
+#uci set network.usbwan.proto='dhcp'
+#uci set network.usbwan.device='eth0'
+
+
+uci commit dhcp
+uci commit network
+uci commit wireless
+uci commit
+#不用重启network，源码自带
+exit 0
