2206da37dbc6e217c0e295438f8d221424227f7a
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-08-04 01:44:36 +0800

    Update 998-ipq60xx.sh

--- patch/998-ipq60xx.sh
+++ patch/998-ipq60xx.sh
@@ -8,8 +8,8 @@ sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/video/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/qualcommax/d' /etc/apk/repositories.d/distfeeds.list
 sed -i 's#downloads.immortalwrt.org#mirrors.vsean.net/openwrt#g' /etc/apk/repositories.d/distfeeds.list
-sed -i '$a https://mirrors.vsean.net/openwrt/releases/25.12.0/targets/qualcommax/ipq60xx/kmods/6.12.87-1-6f890802eaff7c9b13ea5a148e6d0e9d/packages.adb' /etc/apk/repositories.d/distfeeds.list
-sed -i '$a https://mirrors.vsean.net/openwrt/releases/25.12.0/targets/qualcommax/ipq60xx/packages/packages.adb' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.vsean.net/openwrt/releases/25.12.1/targets/qualcommax/ipq60xx/kmods/6.12.94-1-cc8a6bc694108a2afcd04d06958f056f/packages.adb' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.vsean.net/openwrt/releases/25.12.1/targets/qualcommax/ipq60xx/packages/packages.adb' /etc/apk/repositories.d/distfeeds.list
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
